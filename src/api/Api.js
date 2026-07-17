0e5c8c043869e2441ed566b28c7924871cead136
finoob-fasal <finoob66800@gmail.com>
2026-07-17 15:55:58 +0530

    api.jsx

--- src/api/Api.js
+++ src/api/Api.js
@@ -4,4 +4,19 @@ const api = axios.create({
   baseURL: "http://localhost:8000/api/admin/",
 });
 
+api.interceptors.request.use((config) => {
+  // Skip auth header for public endpoints
+  if (config.url === "staff/login/" || config.url === "staff/signup/") {
+    return config;
+  }
+
+  const token = localStorage.getItem("access_token");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
 export default api;
