3a2fa8246b259ac8866f753662c8e3bc8ae1dec5
finoob-fasal <finoob66800@gmail.com>
2026-08-14 11:57:55 +0530

    meta

--- src/components/settings/Notification/MetaComponent.jsx
+++ src/components/settings/Notification/MetaComponent.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { getMetaConnectUrl, getMetaStatus } from "../../../api/meta";
 import { Link2 } from "lucide-react";
 
