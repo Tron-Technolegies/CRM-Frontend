e43ca922d5e3d96d2c531a511185c8b6e4d69f29
finoob-fasal <finoob66800@gmail.com>
2026-08-14 11:02:19 +0530

    meta import

--- src/components/settings/Notification/Metaintegration.jsx
+++ src/components/settings/Notification/Metaintegration.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link2 } from "lucide-react";
-import { getMetaConnectUrl, getMetaStatus } from "../../../api/meta";
+import { getMetaConnectUrl, getMetaStatus } from "../../api/Meta";
 
 const MetaIntegration = () => {
   const [isConnected, setIsConnected] = useState(false);
