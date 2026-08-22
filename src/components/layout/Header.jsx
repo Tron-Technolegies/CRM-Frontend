89108425ff953ab03329ee8d7d6c088742226331
finoob-fasal <finoob66800@gmail.com>
2026-08-22 18:01:08 +0530

    updated

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -130,7 +130,7 @@ export default function Header({ setSidebarOpen }) {
             <Bell size={20} />
 
             {unreadCount > 0 && (
-                <span className="absolute top-2 right-2 w-5 h-5 text-[11px] font-semibold bg-red-500 text-white rounded-full flex items-center justify-center">
+                <span className="absolute top-1 right-2 w-4 h-4 text-[11px] font-semibold bg-red-500 text-white rounded-full flex items-center justify-center">
                     {unreadCount > 9 ? "9+" : unreadCount}
                 </span>
             )}
